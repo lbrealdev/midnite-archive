d87484065cfbcd9cda8f7163fd5bbe2e158cbd9c
lbrealdev <lbrealdeveloper@gmail.com>
2025-08-31 19:00:22 +0200

    test: download videos #01

--- scripts/download_from_list.sh
+++ scripts/download_from_list.sh
@@ -1,4 +1,35 @@
 #!/bin/bash
 
-CHANNEL_LIST="$1"
+CHANNEL_LIST_FILE="$1"
 
+usage() {
+  echo "Usage: $0 <youtube-channel-list>.txt"
+  exit 1
+}
+
+# Check if there enough arguments
+if [ "$#" -lt 1 ]; then
+  usage
+fi
+
+echo "########################################"
+echo "     YouTube Channel Download List      "
+echo "########################################"
+
+if [[ ! -f "$CHANNEL_LIST_FILE" ]]; then
+  echo "The file $CHANNEL_LIST_FILE was not found!"
+  exit 1
+fi
+
+YT_CHANNEL_LIST_FILE="$CHANNEL_LIST_FILE"
+
+printf "\nYouTube channel list: %s\n" "$CHANNEL_LIST"
+
+# -P '~/Desktop/yt-dlp-videos/videos/'
+
+echo "Downloading from list..."
+echo ""
+
+yt-dlp -cw -o "%(title)s-%(id)s.%(ext)s" -a "$YT_CHANNEL_LIST_FILE" --embed-thumbnail --write-description --embed-metadata
+
+echo "Done!"
